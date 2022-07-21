a3f0458c8b51f1f9510e78e0c9ad68a6f962092c
Alimaa Hashim <alimaa@Alimaas-MacBook-Air.local>
2022-07-21 11:31:49 +0100

    Implicit Conversion

---
+++ src/main/scala/MethodsFunctionsImplicitConversions/ImplicitConversion.scala
@@ -0,0 +1,10 @@
+package MethodsFunctionsImplicitConversions
+
+case class Currency(code: String, amount: Double, toUSD: Double)
+object ImplicitConversion extends App {
+
+  // money "USD 100" - convert string
+//  def stringToCurrency(money: String): Currency = {
+//
+//  }
+}
